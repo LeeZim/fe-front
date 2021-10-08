6d7b1f9ca8f76a5a7d9555bc87770e8ccae0c3db
lizeming <12785223@qq.com>
2021-10-08 08:40:00 +0800

    feat(deps): 初始化完成

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 
+// eslint-disable-next-line no-undef
 defineProps<{ msg: string }>()
 
 const count = ref(0)
@@ -19,9 +20,7 @@ const count = ref(0)
   <p>See <code>README.md</code> for more information.</p>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"> Vite Docs </a>
     |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
